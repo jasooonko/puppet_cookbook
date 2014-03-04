65b27d2d11a406bfa86bd67c7d45475af26f6ab4
jason.ko <jason.ko@mlb.com>
2014-03-03 23:28:55 -0500

    add pull update cron function

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,6 +1,21 @@
 class puppet {
   file { '/usr/local/bin/papply':
-    source => '/root/puppet/modules/puppet/manifests/files/papply.sh', 
+    source => 'puppet:///modules/puppet/papply.sh', 
     mode   => '0755',
+  }
+  file { '/usr/local/bin/pull-updates':
+    source => 'puppet:///modules/puppet/pull-updates.sh',
+    mode   => '0755',
+  }
+  file { '/root/.ssh/id_rsa':
+    source => '/root/.ssh/root',
+    mode   => '0600',
+  }
+  cron { 'run-puppet':
+    ensure  => 'present',
+    user    => 'root',
+    command =>  '/usr/local/bin/pull-updates',
+    minute  =>  '*/10',
+    hour    =>  '*',
   }
 }
